c43ca76ca9cdfd67b2f7d78555b2aa4f0612138a
Robert Cohn <rscohn2@gmail.com>
2016-08-20 18:39:33 +0000

    make image name be idp3.ubuntu

--- README.md
+++ README.md
@@ -4,10 +4,14 @@ Build/test/publish docker images for intel python and latest ubuntu
 LTS (16.04 when I wrote this).
 
 It uses miniconda as the installer and creates a conda environment in
-/usr/local/miniconda3/envs/idp. Start a docker container for intel python with:
+/usr/local/miniconda3/envs/idp. Start a docker container for intel python 3.5 with:
 
-    docker run -i -t rscohn2/idp3:ubuntu.2017b1 /usr/local/miniconda3/envs/idp/bin/python
+    docker run -i -t rscohn2/idp3 /idp/bin/python
 
 Builds:
+- rsochn2/idp2
+- rsochn2/idp2:latest
 - rscohn2/idp2:ubuntu.2017b1
+- rsochn2/idp3
+- rsochn2/idp3:latest
 - rscohn2/idp3:ubuntu.2017b1
--- build.py
+++ build.py
@@ -1,15 +1,18 @@
 import sys
 import subprocess
 
+dkr_acct = 'rscohn2'
+
 subprocess.check_call('docker login -u $DOCKER_USER -p $DOCKER_PASSWORD',shell=True)
 ret = 0
 for version in ['2','3']:
     try:
         print('Building %s' % version)
-        tag = 'rscohn2/idp%s:ubuntu.2017b1' % version
-        subprocess.check_call('docker build --build-arg PYVER=%s -t %s .' % (version,tag), shell=True)
-        subprocess.check_call('docker push %s' % tag,shell=True)
+        repo = '%s/idp%s.ubuntu' % (dkr_acct,version)
+        tags = '-t %s:2017b1 -t %s:latest' % (repo, repo)
+        subprocess.check_call('docker build --build-arg PYVER=%s %s .' % (version,tags), shell=True)
+        subprocess.check_call('docker push %s' % repo,shell=True)
     except:
-        print('Failed')
+        print('Failed building python%s' % version)
         ret = 1
 sys.exit(ret)
